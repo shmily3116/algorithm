11f8092873587687bcf6cde8ac73620cb558bc10
Liang <liangxz0731@foxmail.com>
2018-03-28 21:19:38 +0800

    insertion sorting

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,11 @@
 package main;
 
+import sorting.InsertionSorting;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
-
+        InsertionSorting is = new InsertionSorting();
     }
 }
